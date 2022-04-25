40bcf90cb4b87ba23e50dd3569501f7eb50d3509
jaintarun286 <jaintarun286@gmail.com>
2022-04-26 01:35:44 +0530

    verify email functionality implement

--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { SET_CURRENT_USER, AUTH_LOADING, LOGIN_SUCCESS, LOGIN_ERROR, SIGNUP_SUCCESS, SIGNUP_ERROR } from './types';
+import { SET_CURRENT_USER, AUTH_LOADING, LOGIN_SUCCESS, LOGIN_ERROR, SIGNUP_SUCCESS, SIGNUP_ERROR, RESEND_EMAIL_SUCCESS } from './types';
 import store from "./../store";
 import getCurrentHost from "./../constants/index";
 import { authHeader } from './authHeader';
@@ -43,6 +43,28 @@ export const SignupAction = (data) => async (dispatch) => {
         .then(response => {
             console.log("response", response)
             dispatch(signUpSuccess(response));
+        })
+        .catch(error => {
+            dispatch(signUpError(error.response.data));
+        });
+}
+
+export function ResendEmailSuccess(response) {
+    return {
+        type: RESEND_EMAIL_SUCCESS,
+        payload: response,
+    };
+}
+
+export const ResendEmailAction = (data) => async (dispatch) => {
+    dispatch(authRequest());
+    axios
+        .post(getCurrentHost() + "/resend-register-email", data, {
+            headers: authHeader(true),
+        })
+        .then(response => {
+            console.log("response", response)
+            dispatch(ResendEmailSuccess(response));
         })
         .catch(error => {
             dispatch(signUpError(error.response.data));
--- src/views/pages/VerifyEmail/VerifyEmail.js
+++ src/views/pages/VerifyEmail/VerifyEmail.js
@@ -1,17 +1,39 @@
+import { useState, useEffect } from "react"
 import Button from "../../Components/Common/Buttons/Buttons";
-import {Link, useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Header from "../../Components/FrontHeader/FrontHeader";
 import '../Login/Login.css';
+import { ResendEmailAction } from "./../../../redux/actions/authAction"
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+toast.configure();
 const VerifyEmail = (props) => {
-    console.log("props", props.location.state)
+    const dispatch = useDispatch()
+    const verifyEmailSelector = useSelector(state => state)
     const history = useHistory();
-    const validateUser = localStorage.getItem("userType")
+    const [submitClick, setSubmitClick] = useState(false);
 
-    const VerifyEmailSubmit = () => {
-        history.push(`${validateUser === "Patient" ? "/patient" : validateUser === "Trial_Clinic" ? "/trial-clinic" : validateUser === "Physician" ? "/physician" : validateUser === "Pharmaceutical_Companies" ? "/trial-sponsors" : null}/complete-profile`)
+    if (props.location.state === undefined || props.location.state === null) {
+        history.push("/sign-up")
     }
 
+    useEffect(() => {
+        if (submitClick === true) {
+            if (Object.keys(verifyEmailSelector.auth.user).length !== 0 && verifyEmailSelector.auth.loading === false) {
+                toast.success(verifyEmailSelector.auth.user.data.message, { theme: "colored" })
+            } else if (Object.keys(verifyEmailSelector.auth.error).length !== 0 && verifyEmailSelector.auth.loading === false) {
+                let err = verifyEmailSelector.auth?.error?.message;
+                toast.error(err, { theme: "colored" });
+            }
+        }
+    }, [verifyEmailSelector]);
+
+    const HandleResendEmail = () => {
+        dispatch(ResendEmailAction({email: props.location.state.email}))
+        setSubmitClick(true)
+    }
     return (
         <>
             <Header className="innerPageHeader" />
@@ -27,9 +49,24 @@ const VerifyEmail = (props) => {
                                 <img src="/images/email-verification.svg" height="300" alt="Verify Your Email" />
                             </div>
                             <div className="form-group text-center">
-                                <Button isButton="true" BtnType="submit" BtnColor="green w-100" BtnText="Continue" onClick={VerifyEmailSubmit} />
+                                <Button
+                                    isButton="true"
+                                    BtnType="submit"
+                                    BtnColor="green w-100"
+                                    BtnText="Continue"
+                                    onClick={() => history.push("/login")}
+                                />
                             </div>
-                            <p className="create-account">Not received Email? <Link to="/">Resend Email</Link></p>
+                            <p className="create-account d-flex align-items-center justify-content-center">
+                                Not received Email?
+                                <Button
+                                    isButton="true"
+                                    BtnType="button"
+                                    BtnColor="sm btn-text p-0 ms-1"
+                                    BtnText="Resend Email"
+                                    onClick={HandleResendEmail}
+                                />
+                            </p>
                         </form>
                     </div>
                 </div>
