b9ca5bc173396b08c825a6a900dd7a781236242b
Tarun Jain <tarun.jain@arkasoftwares.com>
2022-04-25 19:10:35 +0530

    create loader spinner

---
+++ src/views/Components/Common/Loader/LoaderSpinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Loader.css"
+
+const LoaderSpinner = () => {
+    return (
+        <>
+        &nbsp;<span className="spinner-border d-inline-block" />
+        </>
+    );
+};
+
+export default LoaderSpinner;
