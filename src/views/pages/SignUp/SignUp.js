b7e00a561553fccd4e2464ff021b9d0e5582dd68
Tarun Jain <tarun.jain@arkasoftwares.com>
2022-04-25 19:09:52 +0530

    remove unwated code

--- src/views/pages/SignUp/SignUp.js
+++ src/views/pages/SignUp/SignUp.js
@@ -8,7 +8,7 @@ import Header from "../../Components/FrontHeader/FrontHeader";
 import "../Login/Login.css";
 
 const SignUp = () => {
-    
+
 
     const handleSelect = (key) => {
         console.log("key", key)
@@ -26,60 +26,58 @@ const SignUp = () => {
                         <p>A few clicks away from Creating your account</p>
                     </div>
                     <div className="authentication-bx sign-up-authentication">
-                        <form autoComplete="off">
-                            <Tabs defaultActiveKey="Patient" id="uncontrolled-tab-example" className="selectUserType" onSelect={handleSelect}>
-                                <Tab eventKey="Patient"
-                                    title={
-                                        <>
-                                            <img src="/images/patient.svg" alt="Patient" />
-                                            <h2>Patient</h2>
-                                            <i></i>
-                                        </>
-                                    }
-                                >
-                                    <Patient />
-                                </Tab>
+                        <Tabs defaultActiveKey="Patient" id="uncontrolled-tab-example" className="selectUserType" onSelect={handleSelect}>
+                            <Tab eventKey="Patient"
+                                title={
+                                    <>
+                                        <img src="/images/patient.svg" alt="Patient" />
+                                        <h2>Patient</h2>
+                                        <i></i>
+                                    </>
+                                }
+                            >
+                                <Patient />
+                            </Tab>
 
-                                <Tab eventKey="Trial_Clinic"
-                                    title={
-                                        <>
-                                            <img src="/images/trial-clinic.svg" alt="Patient" />
-                                            <h2>Trial Clinic</h2>
-                                            <i></i>
-                                        </>
-                                    }
-                                >
-                                    <TrialClinic />
-                                </Tab>
+                            <Tab eventKey="Trial_Clinic"
+                                title={
+                                    <>
+                                        <img src="/images/trial-clinic.svg" alt="Patient" />
+                                        <h2>Trial Clinic</h2>
+                                        <i></i>
+                                    </>
+                                }
+                            >
+                                <TrialClinic />
+                            </Tab>
 
-                                <Tab eventKey="Physician"
-                                    title={
-                                        <>
-                                            <img src="/images/physician.svg" alt="Patient" />
-                                            <h2>Physician</h2>
-                                            <i></i>
-                                        </>
-                                    }
-                                >
-                                    <Physician />
-                                </Tab>
+                            <Tab eventKey="Physician"
+                                title={
+                                    <>
+                                        <img src="/images/physician.svg" alt="Patient" />
+                                        <h2>Physician</h2>
+                                        <i></i>
+                                    </>
+                                }
+                            >
+                                <Physician />
+                            </Tab>
 
-                                <Tab eventKey="Pharmaceutical_Companies"
-                                    title={
-                                        <>
-                                            <img src="/images/pharma-companies.svg" alt="Patient" />
-                                            <h2>Clinical Trial Sponsor</h2>
-                                            <i></i>
-                                        </>
-                                    }
-                                >
-                                    <PharmaCompanies />
-                                </Tab>
-                            </Tabs>
-                            <p className="create-account">
-                                Already have any account? <Link to="/login">Login</Link>
-                            </p>
-                        </form>
+                            <Tab eventKey="Pharmaceutical_Companies"
+                                title={
+                                    <>
+                                        <img src="/images/pharma-companies.svg" alt="Patient" />
+                                        <h2>Clinical Trial Sponsor</h2>
+                                        <i></i>
+                                    </>
+                                }
+                            >
+                                <PharmaCompanies />
+                            </Tab>
+                        </Tabs>
+                        <p className="create-account">
+                            Already have any account? <Link to="/login">Login</Link>
+                        </p>
                     </div>
                 </div>
             </section>
