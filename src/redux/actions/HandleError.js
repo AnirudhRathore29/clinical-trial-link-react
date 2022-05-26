5dcbdc2e7bcbfe57cba115cc86133b8dc3fbd3f0
Tarun Jain <tarun.jain@arkasoftwares.com>
2022-05-26 19:02:32 +0530

    unauthorized error handle

--- src/redux/actions/HandleError.js
+++ src/redux/actions/HandleError.js
@@ -5,10 +5,10 @@ const HandleError = error => {
     console.log("error", error)
     toast.configure();
     if (error.code === "UNAUTHORIZED_ACCESS" || error.code === "LOGOUT") {
-        //localStorage.removeItem("auth_security");
-        // localStorage.removeItem("user");
-        //toast.error(error.message, { theme: "colored" })
-        //window.location.href = "/login"
+        localStorage.removeItem("auth_security");
+        localStorage.removeItem("user");
+        toast.error(error.message, { theme: "colored" })
+        window.location.href = "/login"
     }
 };
 
