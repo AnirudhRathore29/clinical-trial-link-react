448f542fa44386c85d3384ebf174de15879637ca
jaintarun286 <jaintarun286@gmail.com>
2022-05-29 01:02:55 +0530

    role condiiton remove

--- src/views/private-route/PrivateRoute.js
+++ src/views/private-route/PrivateRoute.js
@@ -4,19 +4,20 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const PrivateRoute = ({ accessRole, component: Component, auth, ...rest }) => {
-	
+	// && auth.user.role === accessRole 
+	console.log("auth.user.role === accessRole ", auth.user.role , accessRole )
 	return (
 		<Route
 			{...rest}
-			render={(props) => (auth.isAuthenticated === true && auth.user.role === accessRole ? <Component {...props} /> : <Redirect to="/login" />)}
+			render={(props) => (auth.isAuthenticated === true ? <Component {...props} /> : <Redirect to="/login" />)}
 		/>
 	);
 };
 
 // export default PrivateRoute
-PrivateRoute.propTypes = {
-	auth: PropTypes.object.isRequired
-};
+// PrivateRoute.propTypes = {
+// 	auth: PropTypes.object.isRequired
+// };
 
 const mapStateToProps = (state) => ({
 	auth: state.auth
