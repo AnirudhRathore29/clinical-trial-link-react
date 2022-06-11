7b60b43e46bef285d13843c24f88a50f7c28ea40
jaintarun286 <jaintarun286@gmail.com>
2022-06-12 02:48:42 +0530

    added some regex

--- src/utils/ValidationRegex.js
+++ src/utils/ValidationRegex.js
@@ -5,3 +5,7 @@ export const MOBILE_NUMBER_REGEX = /^[0-9]{10}$/;
 export const PASSWORD_REGEX = /((?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-+_!@#$%^&*.,?]))/;
 
 export const ALLOW_LETTERS_ONLY = /^[a-zA-Z\s]*$/;
+
+export const BANK_ACCOUNT_REGEX = /^\d{9,18}$/;
+
+export const ROUTING_NUMBER_REGEX = /^((0[0-9])|(1[0-2])|(2[1-9])|(3[0-2])|(6[1-9])|(7[0-2])|80)([0-9]{7})$/
