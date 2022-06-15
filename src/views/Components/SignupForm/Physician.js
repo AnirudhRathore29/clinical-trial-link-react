72cc75aa5abbc088a59cc888251d0284d9971897
jaintarun286 <jaintarun286@gmail.com>
2022-06-16 01:54:50 +0530

    first name and last name added in physician signup form

--- src/views/Components/SignupForm/Physician.js
+++ src/views/Components/SignupForm/Physician.js
@@ -8,7 +8,7 @@ import RadioBtn from "../Common/RadioBtn/RadioBtn";
 import { SignupAction } from "./../../../redux/actions/authAction"
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { isValidPhoneNumber, isValidEmailAddress, isValidPassword } from "./../../Components/Validation/Validation"
+import { isValidPhoneNumber, isValidEmailAddress, isValidPassword, isValidOnlyLetters } from "./../../Components/Validation/Validation"
 
 const Physician = () => {
     const dispatch = useDispatch()
@@ -16,6 +16,8 @@ const Physician = () => {
     const physicianSelector = useSelector(state => state)
     const [submitClick, setSubmitClick] = useState(false);
     const [Formdata, setFormdata] = useState({
+        first_name: "",
+        last_name: "",
         email: "",
         phone_number: "",
         password: "",
@@ -41,10 +43,10 @@ const Physician = () => {
             if (Object.keys(physicianSelector.auth.user).length !== 0 && physicianSelector.auth.loading === false) {
                 toast.success(physicianSelector.auth.user.data.message, { theme: "colored" })
                 history.push({
-                	pathname: '/verify-email',
-                	state: physicianSelector.auth.user.data.data
+                    pathname: '/verify-email',
+                    state: physicianSelector.auth.user.data.data
                 });
-            } else if(Object.keys(physicianSelector.auth.error).length !== 0 && physicianSelector.auth.loading === false){
+            } else if (Object.keys(physicianSelector.auth.error).length !== 0 && physicianSelector.auth.loading === false) {
                 let err = physicianSelector.auth?.error?.message;
                 toast.error(err, { theme: "colored" });
             }
@@ -65,8 +67,16 @@ const Physician = () => {
     const Vaildation = (value) => {
         const isPhoneVaild = isValidPhoneNumber(value.phone_number);
         const isEmailVaild = isValidEmailAddress(value.email);
+        const isFirstNameVaild = isValidOnlyLetters(value.first_name, "first name")
+        const isLastNameVaild = isValidOnlyLetters(value.last_name, "last name")
         const isPasswordVaild = isValidPassword(value.password);
-        if (!isEmailVaild.status) {
+        if (!isFirstNameVaild.status) {
+            toast.error(isFirstNameVaild.message, { theme: "colored" })
+            return false
+        } else if (!isLastNameVaild.status) {
+            toast.error(isLastNameVaild.message, { theme: "colored" })
+            return false
+        } else if (!isEmailVaild.status) {
             toast.error(isEmailVaild.message, { theme: "colored" })
             return false
         } else if (!isPhoneVaild.status) {
@@ -83,6 +93,8 @@ const Physician = () => {
         event.preventDefault();
         const regData = {
             role: "4",
+            first_name: Formdata.first_name,
+            last_name: Formdata.last_name,
             email: Formdata.email,
             phone_number: Formdata.phone_number,
             password: Formdata.password,
@@ -99,6 +111,28 @@ const Physician = () => {
     return (
         <Form onSubmit={handleSignUPSubmit} autoComplete="off">
             <div className="row">
+                <div className="col-lg-6">
+                    <InputText
+                        type="text"
+                        name="first_name"
+                        defaultValue={Formdata.first_name}
+                        placeholder="Full Name"
+                        required="required"
+                        labelText="First Name"
+                        onChange={onChange}
+                    />
+                </div>
+                <div className="col-lg-6">
+                    <InputText
+                        type="text"
+                        name="last_name"
+                        defaultValue={Formdata.last_name}
+                        placeholder="Last Name"
+                        required="required"
+                        labelText="Last Name"
+                        onChange={onChange}
+                    />
+                </div>
                 <div className="col-lg-6">
                     <InputText
                         type="email"
