2211176a6460006d65377fe961054e341ebd467c
Tarun Jain <tarun.jain@arkasoftwares.com>
2022-07-19 19:48:12 +0530

    firebase chat implemention

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: 'AIzaSyC3DXdLs93NgMBxiHZVJ_HIobFY-sM0zKU',
-    authDomain: 'clinical-trial-7fa28.firebaseapp.com',
-    projectId: 'clinical-trial-7fa28',
-    storageBucket: 'clinical-trial-7fa28.appspot.com',
-    messagingSenderId: '6063884775',
-    appId: '1:6063884775:web:fae1bfef5886ad27609542',
-    measurementId: 'G-SZZ0WTS0VQ',
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MESSAGING_ID,
 })
 
 const db = firebaseApp.firestore()
