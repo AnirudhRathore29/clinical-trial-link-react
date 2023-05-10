78961b874fc101043d9e788f294121c897af58ba
Anirudh rathore <96767807+AnirudhRathore29@users.noreply.github.com>
2023-05-10 17:25:21 +0530

    Update after_install.sh
    done

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu/server
 
 # install dependencies
-npm install --legacy-peer-deps
+npm install
 
 # install create-react-app and react-scripts
 # without react-scripts application cannot be started
